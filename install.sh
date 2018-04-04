8bedb56391db081043aa8371c2a761a795dead67
Soubhagya Rath <soubhagya@localpeep.com>
2018-04-04 13:22:14 +0530

    Fixed crontab edit line; remove existing files before running wget

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 cd /etc/config
+rm hopcloud
 wget https://raw.githubusercontent.com/Hopbox/telemetry/master/hopcloud
-mkdir telemetry
-cd telemetry
+mkdir /etc/config/telemetry
+cd /etc/config/telemetry
+rm telemetry.sh
 wget https://raw.githubusercontent.com/Hopbox/telemetry/master/telemetry.sh
 chmod +x telemetry.sh
-echo " * * * * /etc/config/telemetry/telemetry.sh" >> /etc/crontabs/root
+echo "* * * * * /etc/config/telemetry/telemetry.sh" >> /etc/crontabs/root
